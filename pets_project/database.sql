82f2706303ed68fd881ed2b049918198667ace06
Tanner Smith <tanner.ssu@gmail.com>
2020-02-07 15:41:03 -0600

    added db

---
+++ pets_project/database.sql
@@ -0,0 +1,34 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema pets
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema pets
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `pets` DEFAULT CHARACTER SET utf8 ;
+USE `pets` ;
+
+-- -----------------------------------------------------
+-- Table `pets`.`pets`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `pets`.`pets` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `type` VARCHAR(45) NULL DEFAULT NULL,
+  `created_at` DATETIME NULL DEFAULT NULL,
+  `updated_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
