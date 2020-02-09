80356b0325993370b7969ecdb41c7e3b15b86dcb
Tanner Smith <tanner.ssu@gmail.com>
2020-02-08 22:40:30 -0600

    added password and email regex validation

--- basic_reg/server.py
+++ basic_reg/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, flash, session
 from mysqlconnection import connectToMySQL
-
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+strongRegex = re.compile(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})")
 app = Flask(__name__)
 app.secret_key = "mysecretkeyissupersecret"
 
@@ -10,6 +12,7 @@ def index():
 
 @app.route('/process', methods=["POST"])
 def process():
+    
     is_valid = True
     if len(request.form['first_name']) < 1:
         is_valid = False
@@ -20,9 +23,14 @@ def process():
     if len(request.form['email']) < 2:
         is_valid = False
         flash("Email needs to be valid.")
+    if not EMAIL_REGEX.match(request.form['email']):    # test whether a field matches the pattern
+        flash("Invalid email address!")
     if len(request.form["password"]) < 5:
         is_valid = False
         flash("Password must be at least 5 characters.")
+    if not strongRegex.match(request.form['password']):
+        flash("Password must contain at least one lowercase, one uppercase, one numeric, one special character, grandma's banana bread recipe, and a blood sacrifice.")
+        is_valid = False
     if request.form["password"] != request.form["conf_password"]:
         is_valid = False
         flash("Passwords must match.")
